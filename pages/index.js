e8dcd43fd9e71216bfb2e7da4723addcb7d9d1b9
naielv <109038805+naielv@users.noreply.github.com>
2022-09-23 20:53:43 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,24 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
 import JokeBlock from "@components/JokeBlock";
+import LoginBtn from "@components/login-btn";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Sketaria Web</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="Sketaria Web" />
         <hr />
         <p className="description">
           Here's an example of a Netlify Form! When you fill this out, the
           submissions can be found in the Netlify Admin site.
         </p>
+        <LoginBtn />
         <FeedbackForm />
         <JokeBlock />
       </main>
