abc3c180d99629d5fcca1cc1cbd798759faeabfd
Naiel V <naielvarona@gmail.com>
2022-09-24 10:19:05 +0200

    v2.2.1.1

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ export default function Header({ title }) {
   return (
     <>
       <h1 className="title">{title}</h1>
+      <a href="/api/auth/login">Login</a>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Header from "@components/Header"
 export default function Home() {
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      <TestPageOne></TestPageOne>
       <Header title="Sketaria Web" />
     </div>
   )
