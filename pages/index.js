cb80167d7b8f9e85dcb6e176576c9a2423321ed4
Naiel V <naielvarona@gmail.com>
2022-09-24 09:58:33 +0200

    v2.2

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
       <TestPageOne></TestPageOne>
+      <Header title="Sketaria Web" />
     </div>
   )
 }
